44374b8a510667cc9172ccb2fa0deeb324ae8736
frank.leo <1542874601@qq.com>
2024-01-04 15:18:11 +0800

    temp:临时节点标记 星期四 下午 3:18:11 一月 4日 2024 修改了 src/statics/swagger/swagger-initializer.js

--- src/statics/swagger/swagger-initializer.js
+++ src/statics/swagger/swagger-initializer.js
@@ -5,6 +5,9 @@ window.onload = function () {
   window.ui = SwaggerUIBundle({
     url: "/docs/swagger.json",
     dom_id: '#swagger-ui',
+    defaultModelRendering: "model",
+    defaultModelsExpandDepth: 10,
+    defaultModelExpandDepth: 10,
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
